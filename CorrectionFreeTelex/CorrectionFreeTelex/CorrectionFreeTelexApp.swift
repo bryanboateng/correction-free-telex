96cc9ac5ed658f1c28ea91d56b017d4160ae07b7
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2024-12-25 01:05:56 +0000

    Create Project

---
+++ CorrectionFreeTelex/CorrectionFreeTelex/CorrectionFreeTelexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CorrectionFreeTelexApp.swift
+//  CorrectionFreeTelex
+//
+//  Created by Bryan Oppong-Boateng on 25.12.24.
+//
+
+import SwiftUI
+
+@main
+struct CorrectionFreeTelexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
