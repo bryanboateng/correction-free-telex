e3220d84a65c6d5aa4bb26022181f4e4c9fa691c
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2024-12-25 01:06:43 +0000

    Delete header and format

--- CorrectionFreeTelex/CorrectionFreeTelex/ContentView.swift
+++ CorrectionFreeTelex/CorrectionFreeTelex/ContentView.swift
@@ -1,24 +1,17 @@
-//
-//  ContentView.swift
-//  CorrectionFreeTelex
-//
-//  Created by Bryan Oppong-Boateng on 25.12.24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	var body: some View {
+		VStack {
+			Image(systemName: "globe")
+				.imageScale(.large)
+				.foregroundStyle(.tint)
+			Text("Hello, world!")
+		}
+		.padding()
+	}
 }
 
 #Preview {
-    ContentView()
+	ContentView()
 }
--- CorrectionFreeTelex/CorrectionFreeTelex/CorrectionFreeTelexApp.swift
+++ CorrectionFreeTelex/CorrectionFreeTelex/CorrectionFreeTelexApp.swift
@@ -1,17 +1,10 @@
-//
-//  CorrectionFreeTelexApp.swift
-//  CorrectionFreeTelex
-//
-//  Created by Bryan Oppong-Boateng on 25.12.24.
-//
-
 import SwiftUI
 
 @main
 struct CorrectionFreeTelexApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
