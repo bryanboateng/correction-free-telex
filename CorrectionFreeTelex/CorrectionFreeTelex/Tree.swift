267fb96a4ba0dac75f633c28d9746cabf6b528b2
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2024-12-26 22:13:48 +0000

    Add tones

--- CorrectionFreeTelex/CorrectionFreeTelex/Tree.swift
+++ CorrectionFreeTelex/CorrectionFreeTelex/Tree.swift
@@ -13,7 +13,12 @@ let rootNodes: [Character: Node] = [
 					"a": Node(
 						mapping: "aa",
 						children: [:]
-					)
+					),
+					"f": Node(mapping: "ầ", children: [:]),
+					"s": Node(mapping: "ấ", children: [:]),
+					"r": Node(mapping: "ẩ", children: [:]),
+					"x": Node(mapping: "ẫ", children: [:]),
+					"j": Node(mapping: "ậ", children: [:]),
 				]
 			),
 			"w": Node(
@@ -22,9 +27,19 @@ let rootNodes: [Character: Node] = [
 					"w": Node(
 						mapping: "aw",
 						children: [:]
-					)
+					),
+					"f": Node(mapping: "ằ", children: [:]),
+					"s": Node(mapping: "ắ", children: [:]),
+					"r": Node(mapping: "ẳ", children: [:]),
+					"x": Node(mapping: "ẵ", children: [:]),
+					"j": Node(mapping: "ặ", children: [:]),
 				]
 			),
+			"f": Node(mapping: "à", children: [:]),
+			"s": Node(mapping: "á", children: [:]),
+			"r": Node(mapping: "ả", children: [:]),
+			"x": Node(mapping: "ã", children: [:]),
+			"j": Node(mapping: "ạ", children: [:]),
 		]
 	),
 	"d": Node(
@@ -50,9 +65,19 @@ let rootNodes: [Character: Node] = [
 					"e": Node(
 						mapping: "ee",
 						children: [:]
-					)
+					),
+					"f": Node(mapping: "ề", children: [:]),
+					"s": Node(mapping: "ế", children: [:]),
+					"r": Node(mapping: "ể", children: [:]),
+					"x": Node(mapping: "ễ", children: [:]),
+					"j": Node(mapping: "ệ", children: [:]),
 				]
-			)
+			),
+			"f": Node(mapping: "è", children: [:]),
+			"s": Node(mapping: "é", children: [:]),
+			"r": Node(mapping: "ẻ", children: [:]),
+			"x": Node(mapping: "ẽ", children: [:]),
+			"j": Node(mapping: "ẹ", children: [:]),
 		]
 	),
 	"o": Node(
@@ -64,7 +89,12 @@ let rootNodes: [Character: Node] = [
 					"o": Node(
 						mapping: "oo",
 						children: [:]
-					)
+					),
+					"f": Node(mapping: "ồ", children: [:]),
+					"s": Node(mapping: "ố", children: [:]),
+					"r": Node(mapping: "ổ", children: [:]),
+					"x": Node(mapping: "ỗ", children: [:]),
+					"j": Node(mapping: "ộ", children: [:]),
 				]
 			),
 			"w": Node(
@@ -73,9 +103,19 @@ let rootNodes: [Character: Node] = [
 					"w": Node(
 						mapping: "ow",
 						children: [:]
-					)
+					),
+					"f": Node(mapping: "ờ", children: [:]),
+					"s": Node(mapping: "ớ", children: [:]),
+					"r": Node(mapping: "ở", children: [:]),
+					"x": Node(mapping: "ỡ", children: [:]),
+					"j": Node(mapping: "ợ", children: [:]),
 				]
-			)
+			),
+			"f": Node(mapping: "ò", children: [:]),
+			"s": Node(mapping: "ó", children: [:]),
+			"r": Node(mapping: "ỏ", children: [:]),
+			"x": Node(mapping: "õ", children: [:]),
+			"j": Node(mapping: "ọ", children: [:]),
 		]
 	),
 	"u": Node(
@@ -87,9 +127,19 @@ let rootNodes: [Character: Node] = [
 					"w": Node(
 						mapping: "uw",
 						children: [:]
-					)
+					),
+					"f": Node(mapping: "ừ", children: [:]),
+					"s": Node(mapping: "ứ", children: [:]),
+					"r": Node(mapping: "ử", children: [:]),
+					"x": Node(mapping: "ữ", children: [:]),
+					"j": Node(mapping: "ự", children: [:]),
 				]
-			)
+			),
+			"f": Node(mapping: "ù", children: [:]),
+			"s": Node(mapping: "ú", children: [:]),
+			"r": Node(mapping: "ủ", children: [:]),
+			"x": Node(mapping: "ũ", children: [:]),
+			"j": Node(mapping: "ụ", children: [:]),
 		]
 	)
 ]
